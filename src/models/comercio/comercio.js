e557e58a3e8e55fe83d838eb9cef6c265a8e4c5f
Lawliet <49668391+alramirez97@users.noreply.github.com>
2021-10-14 23:18:23 -0600

    Algo

--- src/models/comercio/comercio.js
+++ src/models/comercio/comercio.js
@@ -5,8 +5,9 @@ const comercioSchema=Mongoose.Schema({
 
     nombreComercio: { type: String, max: 25, min: 5, required: true },
     propietario: { type: String, max: 15, min: 5, required: true },
-    coordenadas: { type: String, required: true },
-    telefono: { type: String, max: 8, min: 8, require: true },
+    longitud: { type: String,},
+    latitud: { type: String,},
+    telefono: { type: String, max: 15, min: 8, require: true },
     redes_sociales: { type: String, min: 8 },
     categoria: [{
         ref: "Categoria",
